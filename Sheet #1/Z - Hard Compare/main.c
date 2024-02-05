84fb539ff8c57d0d257d56810e9100ebfb507387
bassemalkhashab <bassemalkhashab@gmail.com>
2024-02-05 22:41:43 +0200

    Add the answer of Z-Hard compare

--- Sheet #1/Z - Hard Compare/main.c
+++ Sheet #1/Z - Hard Compare/main.c
@@ -7,6 +7,7 @@ int main()
 {
     int64_t a,b,c,d;
     scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
+    // if (a^b > c^d) then (b*log(a) > d*log(c))
     b*log(a) > d*log(c)? printf("YES") : printf("NO");
     return 0;
 }
